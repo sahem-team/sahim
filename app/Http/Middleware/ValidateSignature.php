18a3e7a1d9fef27daf9b1554c7868c8fe9e7d5d3
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-05 00:04:21 +0300

    Update ValidateSignature.php

--- app/Http/Middleware/ValidateSignature.php
+++ app/Http/Middleware/ValidateSignature.php
@@ -7,7 +7,7 @@ use Illuminate\Routing\Middleware\ValidateSignature as Middleware;
 class ValidateSignature extends Middleware
 {
     /**
-     * The names of the query string parameters that should be ignored.
+     * #The names of the query string parameters that should be ignored.
      *
      * @var array<int, string>
      */
