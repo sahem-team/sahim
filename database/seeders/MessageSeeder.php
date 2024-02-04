e94d0301f661b058cc2e4622d0858ac135457897
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-05 00:45:33 +0300

    Update MessageSeeder.php

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Seeder;
 class MessageSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeds
      */
     public function run(): void
     {
