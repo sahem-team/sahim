63afc3aa8e213ccdf674c93b1fd4779f153f3d3d
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-05 00:14:10 +0300

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -14,7 +14,7 @@ return [
     |
     | This option defines the default log channel that gets used when writing
     | messages to the logs. The name specified in this option should match
-    | one of the channels defined in the "channels" configuration array.
+    | one of the channels defined in the "channels" configuration array.#
     |
     */
 
