b670c43a5f2adb1925348ee1a2783d622fe57ec2
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-05 00:04:03 +0300

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class TrustProxies extends Middleware
 {
     /**
-     * The trusted proxies for this application.
+     * #The trusted proxies for this application.
      *
      * @var array<int, string>|string|null
      */
