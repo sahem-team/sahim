1c265cc0ec1dc5ecd0df7c37b0bac78de0d0cc8d
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-05 00:15:10 +0300

    Update queue.php

--- config/queue.php
+++ config/queue.php
@@ -8,7 +8,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Laravel's queue API supports an assortment of back-ends via a single
-    | API, giving you convenient access to each back-end using the same
+    | API, giving you convenient access to each -back-end using the same
     | syntax for every one. Here you may define a default connection.
     |
     */
