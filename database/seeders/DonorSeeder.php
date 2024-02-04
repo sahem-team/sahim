068eceee8ae16292255ffd263b7b7b22174982f5
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-05 00:45:11 +0300

    Update DonorSeeder.php

--- database/seeders/DonorSeeder.php
+++ database/seeders/DonorSeeder.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Seeder;
 class DonorSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeds
      */
     public function run()
     {
