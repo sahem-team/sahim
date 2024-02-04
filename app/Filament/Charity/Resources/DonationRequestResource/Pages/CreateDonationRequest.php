93208506ad96ebacfd9af88af78dbfc6eff55769
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-04 14:43:58 +0300

    Update CreateDonationRequest.php

--- app/Filament/Charity/Resources/DonationRequestResource/Pages/CreateDonationRequest.php
+++ app/Filament/Charity/Resources/DonationRequestResource/Pages/CreateDonationRequest.php
@@ -1,10 +1,11 @@
+
 <?php
 
 namespace App\Filament\Charity\Resources\DonationRequestResource\Pages;
 
 use App\Filament\Charity\Resources\DonationRequestResource;
 use Filament\Resources\Pages\CreateRecord;
-
+/**/
 class CreateDonationRequest extends CreateRecord
 {
     protected static string $resource = DonationRequestResource::class;
