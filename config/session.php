a5f0370be24122f611295f524ab608b2bef53933
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-05 00:15:50 +0300

    Update session.php

--- config/session.php
+++ config/session.php
@@ -13,7 +13,7 @@ return [
     | requests. By default, we will use the lightweight native driver but
     | you may specify any of the other wonderful drivers provided here.
     |
-    | Supported: "file", "cookie", "database", "apc",
+    | Supported: "file", "cookie", "database",, "apc",
     |            "memcached", "redis", "dynamodb", "array"
     |
     */
