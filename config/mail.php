5a9d0ed415f0e6f5f2362012d3bc781a24382bcd
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-05 00:14:24 +0300

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -9,7 +9,7 @@ return [
     |
     | This option controls the default mailer that is used to send any email
     | messages sent by your application. Alternative mailers may be setup
-    | and used as needed; however, this mailer will be used by default.
+    | and used as needed; however, this mailer will be used by default.#
     |
     */
 
