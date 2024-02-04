35eaac80c1f4c0371e7c24497c23a04abb843df3
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-05 00:02:16 +0300

    Update Authenticate.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class Authenticate extends Middleware
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * Get the path the user should be redirected to when they are not authenticated.#
      */
     protected function redirectTo(Request $request): ?string
     {
