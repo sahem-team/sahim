9313bbcaf0b7a992e31573b7e414d0b1671d2c21
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-05 00:05:47 +0300

    Update Donation.php

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
-
+/**/
 class Donation extends Model implements HasMedia
 {
     use InteractsWithMedia, HasFactory;
