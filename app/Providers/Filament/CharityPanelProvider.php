1d12c9b7290d43c6f1a80f6f691cd76cbc5b95dc
ryuof-turki <155353842+ryuof-turki@users.noreply.github.com>
2024-02-05 00:08:46 +0300

    Update CharityPanelProvider.php

--- app/Providers/Filament/CharityPanelProvider.php
+++ app/Providers/Filament/CharityPanelProvider.php
@@ -19,7 +19,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-
+/**/
 class CharityPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
