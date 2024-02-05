7bdac59ccc92f112201c5f61f389ef198ee80e57
Sahem Team <155353842+sahem-team@users.noreply.github.com>
2024-02-05 18:04:01 +0300

    Update simple-bootstrap-4.blade.php

--- resources/views/vendor/pagination/simple-bootstrap-4.blade.php
+++ resources/views/vendor/pagination/simple-bootstrap-4.blade.php
@@ -1,7 +1,7 @@
 @if ($paginator->hasPages())
     <nav>
         <ul class="pagination">
-            {{-- Previous Page Link --}}
+            {{-- Previous Page Link. --}}
             @if ($paginator->onFirstPage())
                 <li class="page-item disabled" aria-disabled="true">
                     <span class="page-link">@lang('pagination.previous')</span>
