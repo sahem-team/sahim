0e00cd64616a3b14382b0de6f16247ffb52a3ea8
Sahem Team <155353842+sahem-team@users.noreply.github.com>
2024-02-05 18:04:44 +0300

    Update simple-tailwind.blade.php

--- resources/views/vendor/pagination/simple-tailwind.blade.php
+++ resources/views/vendor/pagination/simple-tailwind.blade.php
@@ -1,6 +1,6 @@
 @if ($paginator->hasPages())
     <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-between">
-        {{-- Previous Page Link --}}
+        {{-- Previous Page Link. --}}
         @if ($paginator->onFirstPage())
             <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md">
                 {!! __('pagination.previous') !!}
