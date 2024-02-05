157c46dca0558c72fc741175955f576052544fc7
Sahem Team <155353842+sahem-team@users.noreply.github.com>
2024-02-05 17:57:58 +0300

    Update articles.blade.php

--- resources/views/articles/articles.blade.php
+++ resources/views/articles/articles.blade.php
@@ -21,7 +21,7 @@
         </div>
     </div>
     </aside>
-/**/
+
     <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <div class="mx-auto max-w-screen-md sm:text-center">
