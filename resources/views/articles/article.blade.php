69d883d98f28751564af084300c91172f2430eee
Sahem Team <155353842+sahem-team@users.noreply.github.com>
2024-02-05 17:56:57 +0300

    Update article.blade.php

--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -1,4 +1,4 @@
-/**/@extends('layouts.master')
+@extends('layouts.master')
 @section('content')
     <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
