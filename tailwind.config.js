1853f41ef113996dca973ef16463bfd9bf8f15de
Sahem Team <155353842+sahem-team@users.noreply.github.com>
2024-02-05 17:21:29 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} #*/
 const flowbitePlugin = require("flowbite/plugin");
 const formsPlugin = require("@tailwindcss/forms");
 export default {
