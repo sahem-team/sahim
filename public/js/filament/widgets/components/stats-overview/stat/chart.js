fb3895740b642321c19305c4a38e2e8a5e37c404
Sahem Team <155353842+sahem-team@users.noreply.github.com>
2024-02-05 17:15:15 +0300

    Update chart.js

--- public/js/filament/widgets/components/stats-overview/stat/chart.js
+++ public/js/filament/widgets/components/stats-overview/stat/chart.js
@@ -8,7 +8,7 @@ chart.js/dist/chunks/helpers.segment.mjs:
    * Chart.js v3.9.1
    * https://www.chartjs.org
    * (c) 2022 Chart.js Contributors
-   * Released under the MIT License
+   * Released under the MIT License#
    *)
 
 chart.js/dist/chunks/helpers.segment.mjs:
